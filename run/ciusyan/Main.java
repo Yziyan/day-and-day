c5d9e48d1da2a45a34272a36b43b1a39932de471
Ciusyan <1533460130@qq.com>
2023-05-03 10:39:50 +0800

    计数排序

--- run/ciusyan/Main.java
+++ run/ciusyan/Main.java
@@ -5,11 +5,12 @@ import run.ciusyan._5_2.InsertSort;
 import run.ciusyan._5_2.MergeSort;
 import run.ciusyan._5_2.QuickSort;
 import run.ciusyan._5_2.SelectionSort;
+import run.ciusyan._5_3.CountingSort;
 
 public class Main {
     public static void main(String[] args) {
         int[] array = {10, 20, 31, 41, 98, 65, 21, 53, 8, 33, 87, 43, 29};
-        QuickSort.sort(array);
+        CountingSort.sort(array);
         for (int i : array) {
             System.out.print(i + "_");
         }

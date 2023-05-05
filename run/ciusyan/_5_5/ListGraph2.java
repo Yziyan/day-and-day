06b0034dc58da504bd775b39ef08db034e4e172f
Ciusyan <1533460130@qq.com>
2023-05-05 20:15:43 +0800

    Dijkstra简单实现(单源最短路径算法)

--- run/ciusyan/_5_5/ListGraph2.java
+++ run/ciusyan/_5_5/ListGraph2.java
@@ -14,10 +14,10 @@ import java.util.*;
 public class ListGraph2 <V, E> extends ListGraph<V, E> {
 
     /** 每一幅图都拥有自己的权值管理器 */
-    private WeightManager weightManager;
+    private WeightManager<E> weightManager;
 
     /** 每一幅图都拥有自己的边比较器 */
-    private Comparator<Edge<V, E>> weightComparator = new Comparator<>() {
+    private Comparator<Edge<V, E>> edgeComparator = new Comparator<>() {
         @Override
         public int compare(Edge<V, E> o1, Edge<V, E> o2) {
             return weightManager.compare(o1.weight, o2.weight);
@@ -47,7 +47,7 @@ public class ListGraph2 <V, E> extends ListGraph<V, E> {
         cuts.add(beginVertex);
 
         // 准备一个最小堆，用于不断获取权值最小边
-        MinHeap<Edge<V, E>> minHeap = new MinHeap<>(beginVertex.outEdges, weightComparator);
+        MinHeap<Edge<V, E>> minHeap = new MinHeap<>(beginVertex.outEdges, edgeComparator);
 
         // 堆为空、或者已经切完了
         int vertexSize = vertices.size();
@@ -73,7 +73,7 @@ public class ListGraph2 <V, E> extends ListGraph<V, E> {
         Set<EdgeInfo<V, E>> result = new HashSet<>();
 
         // 准备一个最小堆，用于每一次获取最小权边（直接将所有边入堆）
-        MinHeap<Edge<V, E>> minHeap = new MinHeap<>(edges, weightComparator);
+        MinHeap<Edge<V, E>> minHeap = new MinHeap<>(edges, edgeComparator);
         // 准备一个并查集，用于判断是否有环
         UnionFind<Vertex<V, E>> unionFind = new UnionFind<>();
         // 将所有顶点初始化为一个单独的集合
@@ -98,9 +98,91 @@ public class ListGraph2 <V, E> extends ListGraph<V, E> {
         return result;
     }
 
+    /**
+     * Dijkstra实现的单元最短路径算法：
+     * Dijkstra 是一种基于贪心策略的一种求解单源最短路径的算法，它的核心是不断的理由松弛操作。
+     * 从源点开始，不断的对每一条边进行松弛操作，其实就是更新源点到能到达顶点间的距离，每一次松弛操作的意义在于，尝试找出
+     * 更短的路径，然后每一次松弛完成后，选择所有路径中长度最短的一条路径，这条路径就是源点到对应终点的最短路径。
+     * 它适用于没有负权边的图（有向、无向、无权都可以）
+     * @param src：源点
+     * @return ：源点到其他的点的最短路径长度。如：<B， 40>
+     */
+    public Map<V, E> shortPathDijkstra(V src) {
+        Map<V, E> result = new HashMap<>();
+
+        // 获取源点
+        Vertex<V, E> srcVertex = vertices.get(src);
+        if (srcVertex == null) return result;
+
+        // 记录中间结果
+        Map<Vertex<V, E>, E> paths = new HashMap<>();
+        // 初始化源点到达能到达点的路径长度
+        for (Edge<V, E> edge : srcVertex.outEdges) {
+            paths.put(edge.to, edge.weight);
+        }
+
+        // 只要 paths 还有顶点没有求出最短路径，那就继续求
+        while (!paths.isEmpty()) {
+            // 删除并且获取此次最短路径的顶点
+            Map.Entry<Vertex<V, E>, E> minPath = getMinPath(paths);
+            // 删除它
+            Vertex<V, E> distVertex = minPath.getKey();
+            E minWeight = paths.remove(distVertex);
+            // 记录结果
+            result.put(distVertex.value, minWeight);
+
+            // 将该顶点能到达的边进行松弛操作
+            for (Edge<V, E> outEdge : distVertex.outEdges) {
+                // 但是得先看看这个顶点是否已经计算出来了。
+                if (result.containsKey(outEdge.to.value)) continue;
+
+                // 当前最短路径 + 这条出边
+                E newWeight = weightManager.add(minWeight, outEdge.weight);
+                // 以前的权值
+                E oldWeight = paths.get(outEdge.to);
+
+                if (oldWeight != null && weightManager.compare(newWeight, oldWeight) >= 0) continue;
+                // 来到这这里说明新的权值要小
+
+                paths.put(outEdge.to, newWeight);
+            }
+
+        }
+
+        // 将源点 -> 源点的路径删除掉
+        result.remove(src);
+
+        return result;
+    }
+
+    /**
+     * 获取现有结果中的最小路径
+     * @param paths：所有路径
+     * @return 将 key, value 一起返回
+     */
+    private Map.Entry<Vertex<V, E>, E> getMinPath(Map<Vertex<V, E>, E> paths) {
+        Iterator<Map.Entry<Vertex<V, E>, E>> it = paths.entrySet().iterator();
+        // 默认获取到的第一个键值对就是最小值（前面会判断，所以这里可以放心的取）
+        Map.Entry<Vertex<V, E>, E> minPath = it.next();
+
+        while (it.hasNext()) {
+            Map.Entry<Vertex<V, E>, E> next = it.next();
+
+            if (weightManager.compare(next.getValue(), minPath.getValue()) < 0) {
+                // 说明比最小值还小
+                minPath = next;
+            }
+        }
+
+        return minPath;
+    }
+
     /** 边权值的管理器 */
     public interface WeightManager<E> {
         /** 比较两个边权值的大小 */
         int compare(E w1, E w2);
+
+        /** 将两条边的权值相加 */
+        E add(E w1, E w2);
     }
 }

c8176caea977ee36f0083c92e3f7c4500b3271a7
Ciusyan <1533460130@qq.com>
2023-05-16 16:27:12 +0800

    部分排序

---
+++ run/ciusyan/_5_16/部分排序.java
@@ -0,0 +1,35 @@
+package run.ciusyan._5_16;
+
+/**
+ * https://leetcode.cn/problems/sub-sort-lcci/
+ */
+public class 部分排序 {
+    public int[] subSort(int[] array) {
+        if (array == null || array.length == 0) return new int[] {-1, -1};
+        if (array.length < 2) return array;
+
+        // 从前往后找到最后一个逆序对，就是最后一个乱序的
+        int lastIdx = -1;
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] >= max) {
+                max = array[i];
+            } else {
+                lastIdx = i;
+            }
+        }
+
+        // 从后往前找到最前面一个逆序对，就是第一个乱序的
+        int first = -1;
+        int min = array[array.length - 1];
+        for (int i = array.length - 2; i >= 0; i--) {
+            if (array[i] <= min) {
+                min = array[i];
+            } else {
+                first = i;
+            }
+        }
+
+        return new int[] {first, lastIdx};
+    }
+}
